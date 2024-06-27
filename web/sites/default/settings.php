e93723474e0911a3cb5757ae51b46d656cbd099b
Aldrin Papa <papaaldrin@gmail.com>
2024-06-27 17:03:49 +0800

    Set config sync directory

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -236,6 +236,7 @@ $databases = [];
  * its location.
  */
 # $settings['config_sync_directory'] = '/directory/outside/webroot';
+$settings['config_sync_directory'] = '../config/sync';
 
 /**
  * Settings:
